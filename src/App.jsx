b0af2ae28b32c1cd3778e59e6c3aba8aa94e599a
Zelalem Getahun <zelalemg935@gmail.com>
2025-05-04 05:54:34 -0700

    built the main pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import HomePage from "./pages/HomePage";
 import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
+          <Route path="login" element={<Login />} />
+          <Route path="app" element={<AppLayout />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,18 +1,23 @@
 import { NavLink } from "react-router-dom";
 import Styles from "./PageNav.module.css";
+import Logo from "./Logo";
 
 function PageNav() {
   return (
     <nav className={Styles.nav}>
+      <Logo />
+
       <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
         <li>
           <NavLink to="/Pricing">Pricing</NavLink>
         </li>
         <li>
           <NavLink to="/Product">Product</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login" className={Styles.ctaLink}>
+            Login
+          </NavLink>
         </li>
       </ul>
     </nav>
