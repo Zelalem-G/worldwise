5907cbc10907e4692346f5de4876ae7e77c2a79e
Zelalem Getahun <zelalemg935@gmail.com>
2025-07-17 12:04:16 -0700

    app navigation added

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,3 +1,4 @@
+import { useParams, useSearchParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 const formatDate = (date) =>
@@ -9,6 +10,8 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function City() {
+  const { id } = useParams();
+
   // TEMP DATA
   const currentCity = {
     cityName: "Lisbon",
@@ -16,46 +19,50 @@ function City() {
     date: "2027-10-31T15:59:59.138Z",
     notes: "My favorite city so far!",
   };
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lng = searchParams.get("lng");
+  const lat = searchParams.get("lat");
 
   const { cityName, emoji, date, notes } = currentCity;
 
-  return (
-    <div className={styles.city}>
-      <div className={styles.row}>
-        <h6>City name</h6>
-        <h3>
-          <span>{emoji}</span> {cityName}
-        </h3>
-      </div>
+  return <hi>City {id}</hi>;
+  // return (
+  //   <div className={styles.city}>
+  //     <div className={styles.row}>
+  //       <h6>City name</h6>
+  //       <h3>
+  //         <span>{emoji}</span> {cityName}
+  //       </h3>
+  //     </div>
 
-      <div className={styles.row}>
-        <h6>You went to {cityName} on</h6>
-        <p>{formatDate(date || null)}</p>
-      </div>
+  //     <div className={styles.row}>
+  //       <h6>You went to {cityName} on</h6>
+  //       <p>{formatDate(date || null)}</p>
+  //     </div>
 
-      {notes && (
-        <div className={styles.row}>
-          <h6>Your notes</h6>
-          <p>{notes}</p>
-        </div>
-      )}
+  //     {notes && (
+  //       <div className={styles.row}>
+  //         <h6>Your notes</h6>
+  //         <p>{notes}</p>
+  //       </div>
+  //     )}
 
-      <div className={styles.row}>
-        <h6>Learn more</h6>
-        <a
-          href={`https://en.wikipedia.org/wiki/${cityName}`}
-          target="_blank"
-          rel="noreferrer"
-        >
-          Check out {cityName} on Wikipedia &rarr;
-        </a>
-      </div>
+  //     <div className={styles.row}>
+  //       <h6>Learn more</h6>
+  //       <a
+  //         href={`https://en.wikipedia.org/wiki/${cityName}`}
+  //         target="_blank"
+  //         rel="noreferrer"
+  //       >
+  //         Check out {cityName} on Wikipedia &rarr;
+  //       </a>
+  //     </div>
 
-      <div>
-        <ButtonBack />
-      </div>
-    </div>
-  );
+  //     <div>
+  //  //     <ButtonBack />
+  //     </div>
+  //   </div>
+  // );
 }
 
 export default City;
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,25 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lng = searchParams.get("lng");
+  const lat = searchParams.get("lat");
+
+  return (
+    <div
+      className={styles.mapContainer}
+      onClick={() => {
+        navigate("form");
+      }}
+    >
+      <h1>map</h1>
+      <h2>
+        Position: {lat} {lng}
+      </h2>
+    </div>
+  );
 }
 
 export default Map;
