15b867e2395bf5ce1c5e048a5ab82f81722093df
Zelalem Getahun <zelalemg935@gmail.com>
2025-07-19 04:00:05 -0700

    using contex api for cities

---
+++ src/contexts/CitiesContext.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect, useContext } from "react";
+import { createContext } from "react";
+
+const BASE_URL = "http://localhost:9000";
+
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        console.log("ERROR LOADING DATA IN THE USEEFFECT FETCHCITIES FUNCTION");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  return (
+    <CitiesContext.Provider value={{ cities, isLoading }}>
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined) {
+    throw new Error(" Cities contex is used outside its context");
+  }
+  return context;
+}
+
+export { CitiesProvider, useCities };
